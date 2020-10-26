5c98efdcf48f0a5a347a57a0acbbeda61d664bc0
luiz <cieslakluiz@gmail.com>
2020-10-26 09:40:27 -0300

    prettier

--- .hyper.js
+++ .hyper.js
@@ -3,189 +3,189 @@
 // See https://hyper.is#cfg for all currently supported options.
 
 module.exports = {
-  config: {
-    // choose either `'stable'` for receiving highly polished,
-    // or `'canary'` for less polished but more frequent updates
-    updateChannel: 'stable',
+	config: {
+		// choose either `'stable'` for receiving highly polished,
+		// or `'canary'` for less polished but more frequent updates
+		updateChannel: 'stable',
 
-    // default font size in pixels for all tabs
-    fontSize: 12,
+		// default font size in pixels for all tabs
+		fontSize: 12,
 
-    // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-    // fontFamily: '"Hack Nerd Font", monospace',
+		// font family with optional fallbacks
+		fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+		// fontFamily: '"Hack Nerd Font", monospace',
 
-    // default font weight: 'normal' or 'bold'
-    fontWeight: 'normal',
+		// default font weight: 'normal' or 'bold'
+		fontWeight: 'normal',
 
-    // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: 'bold',
+		// font weight for bold characters: 'normal' or 'bold'
+		fontWeightBold: 'bold',
 
-    // line height as a relative unit
-    lineHeight: 1,
+		// line height as a relative unit
+		lineHeight: 1,
 
-    // letter spacing as a relative unit
-    letterSpacing: 0,
+		// letter spacing as a relative unit
+		letterSpacing: 0,
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+		// terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+		cursorColor: 'rgba(248,28,229,0.8)',
 
-    // terminal text color under BLOCK cursor
-    cursorAccentColor: '#a12345',
+		// terminal text color under BLOCK cursor
+		cursorAccentColor: '#a12345',
 
-    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-    cursorShape: 'BLOCK',
+		// `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+		cursorShape: 'BLOCK',
 
-    // set to `true` (without backticks and without quotes) for blinking cursor
-    cursorBlink: false,
+		// set to `true` (without backticks and without quotes) for blinking cursor
+		cursorBlink: false,
 
-    // color of the text
-    foregroundColor: '#fff',
+		// color of the text
+		foregroundColor: '#fff',
 
-    // terminal background color
-    // opacity is only supported on macOS
-    backgroundColor: '#111',
+		// terminal background color
+		// opacity is only supported on macOS
+		backgroundColor: '#111',
 
-    // terminal selection color
-    selectionColor: 'rgba(248,28,229,0.3)',
+		// terminal selection color
+		selectionColor: 'rgba(248,28,229,0.3)',
 
-    // border color (window, tabs)
-    borderColor: '#333',
+		// border color (window, tabs)
+		borderColor: '#333',
 
-    // custom CSS to embed in the main window
-    css: '',
+		// custom CSS to embed in the main window
+		css: '',
 
-    // custom CSS to embed in the terminal window
-    termCSS: '',
+		// custom CSS to embed in the terminal window
+		termCSS: '',
 
-    // if you're using a Linux setup which show native menus, set to false
-    // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+		// if you're using a Linux setup which show native menus, set to false
+		// default: `true` on Linux, `true` on Windows, ignored on macOS
+		showHamburgerMenu: '',
 
-    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+		// set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
+		// additionally, set to `'left'` if you want them on the left, like in Ubuntu
+		// default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
+		showWindowControls: '',
 
-    // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+		// custom padding (CSS format, i.e.: `top right bottom left`)
+		padding: '12px 14px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#C51E14',
-      green: '#1DC121',
-      yellow: '#C7C329',
-      blue: '#0A2FC4',
-      magenta: '#C839C5',
-      cyan: '#20C5C6',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
-    },
+		// the full list. if you're going to provide the full color palette,
+		// including the 6 x 6 color cubes and the grayscale map, just provide
+		// an array here instead of a color map object
+		colors: {
+			black: '#000000',
+			red: '#C51E14',
+			green: '#1DC121',
+			yellow: '#C7C329',
+			blue: '#0A2FC4',
+			magenta: '#C839C5',
+			cyan: '#20C5C6',
+			white: '#C7C7C7',
+			lightBlack: '#686868',
+			lightRed: '#FD6F6B',
+			lightGreen: '#67F86F',
+			lightYellow: '#FFFA72',
+			lightBlue: '#6A76FB',
+			lightMagenta: '#FD7CFC',
+			lightCyan: '#68FDFE',
+			lightWhite: '#FFFFFF',
+		},
 
-    // // Material Theme settings
-    MaterialTheme: {
-      // Set the theme variant,
-      // OPTIONS: 'Darker', 'Palenight', 'Ocean', ''
-      theme: 'Darker',
+		// // Material Theme settings
+		MaterialTheme: {
+			// Set the theme variant,
+			// OPTIONS: 'Darker', 'Palenight', 'Ocean', ''
+			theme: 'Darker',
 
-      // [Optional] Set the rgba() app background opacity, useful when enableVibrance is true
-      // OPTIONS: From 0.1 to 1
-      backgroundOpacity: '0.3',
+			// [Optional] Set the rgba() app background opacity, useful when enableVibrance is true
+			// OPTIONS: From 0.1 to 1
+			backgroundOpacity: '0.3',
 
-      // [Optional] Set the accent color for the current active tab
-      accentColor: '#64FFDA',
+			// [Optional] Set the accent color for the current active tab
+			accentColor: '#64FFDA',
 
-      // [Optional] Mac Only. Need restart. Enable the vibrance and blurred background
-      // OPTIONS: 'dark', 'ultra-dark', 'bright'
-      // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
-      vibrancy: 'dark',
-    },
+			// [Optional] Mac Only. Need restart. Enable the vibrance and blurred background
+			// OPTIONS: 'dark', 'ultra-dark', 'bright'
+			// NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
+			vibrancy: 'dark',
+		},
 
-    // verminal: {
-    //   fontFamily: '"Ubuntu Mono derivative Powerline"',
-    //   fontSize: 12
-    // },
+		// verminal: {
+		//   fontFamily: '"Ubuntu Mono derivative Powerline"',
+		//   fontSize: 12
+		// },
 
-    // hyperline settings
-    // hyperline: {
-    //   plugins: ['network', 'cpu', 'memory', 'battery'],
-    // },
+		// hyperline settings
+		// hyperline: {
+		//   plugins: ['network', 'cpu', 'memory', 'battery'],
+		// },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // PowerShell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: 'zsh',
+		// the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+		// if left empty, your system's login shell will be used by default
+		//
+		// Windows
+		// - Make sure to use a full path if the binary name doesn't work
+		// - Remove `--login` in shellArgs
+		//
+		// Bash on Windows
+		// - Example: `C:\\Windows\\System32\\bash.exe`
+		//
+		// PowerShell on Windows
+		// - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+		shell: 'zsh',
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-    // by default `['--login']` will be used
-    shellArgs: ['--login'],
+		// for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
+		// by default `['--login']` will be used
+		shellArgs: ['--login'],
 
-    // for environment variables
-    env: {},
+		// for environment variables
+		env: {},
 
-    // set to `false` for no bell
-    bell: 'SOUND',
+		// set to `false` for no bell
+		bell: 'SOUND',
 
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
+		// if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+		copyOnSelect: false,
 
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true,
+		// if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
+		defaultSSHApp: true,
 
-    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-    // selection is present (`true` by default on Windows and disables the context menu feature)
-    // quickEdit: true,
+		// if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
+		// selection is present (`true` by default on Windows and disables the context menu feature)
+		// quickEdit: true,
 
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
+		// URL to custom bell
+		// bellSoundURL: 'http://example.com/bell.mp3',
 
-    // for advanced config flags please refer to https://hyper.is/#cfg
-  },
+		// for advanced config flags please refer to https://hyper.is/#cfg
+	},
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: [
-    // 'hyper-material-theme', // "verminal",
-    'hyper-active-tab',
-    'hyper-highlight-active-pane',
-    'hyper-search',
-    'hyper-opacity',
-    'hyper-tabs-enhanced',
-    'hypercwd',
-    // 'hyperline',
-    'hyperlinks',
-  ],
+	// a list of plugins to fetch and install from npm
+	// format: [@org/]project[#version]
+	// examples:
+	//   `hyperpower`
+	//   `@company/project`
+	//   `project#1.0.1`
+	plugins: [
+		// 'hyper-material-theme', // "verminal",
+		'hyper-active-tab',
+		'hyper-highlight-active-pane',
+		'hyper-search',
+		'hyper-opacity',
+		'hyper-tabs-enhanced',
+		'hypercwd',
+		// 'hyperline',
+		'hyperlinks',
+	],
 
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: [],
+	// in development, you can create a directory under
+	// `~/.hyper_plugins/local/` and include it here
+	// to load it and avoid it being `npm install`ed
+	localPlugins: [],
 
-  keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
-  },
+	keymaps: {
+		// Example
+		// 'window:devtools': 'cmd+alt+o',
+	},
 }
