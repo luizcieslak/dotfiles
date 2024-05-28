15b9cf4c30f21639650e1e7b4ed810d66697573b
Luiz Cieslak <cieslakluiz@gmail.com>
2024-05-28 16:47:06 -0300

    add postgress

--- install.sh
+++ install.sh
@@ -123,6 +123,23 @@ echo "### installing pgadmin4 ###"
 sudo apt install pgadmin4
 echo "### finishedinstalling pgadmin4 ###"
 
+echo "### installing postgresql ###"
+# Import the repository signing key:
+sudo apt install curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+# Create the repository configuration file:
+sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Update the package lists:
+sudo apt update
+
+# Install the latest version of PostgreSQL:
+# If you want a specific version, use 'postgresql-16' or similar instead of 'postgresql'
+sudo apt -y install postgresql
+echo "### finished installing postgresql ###"
+
 echo "### installing docker ###"
 sudo apt-get update
 sudo apt-get install ca-certificates curl
