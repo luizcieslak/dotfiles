d9f60b8cc9bee6428a87ecdfc9d14142035a4cef
Luiz Cieslak <cieslakluiz@gmail.com>
2024-05-28 16:37:38 -0300

    add docker

--- install.sh
+++ install.sh
@@ -123,6 +123,22 @@ echo "### installing pgadmin4 ###"
 sudo apt install pgadmin4
 echo "### finishedinstalling pgadmin4 ###"
 
+echo "### installing docker ###"
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "### finished installing docker ###"
+
 echo "### fix window and linux dual boot time mismatch  ###"
 timedatectl set-local-rtc 1
 
